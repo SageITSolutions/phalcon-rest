cc02de327aca19d5cbb8ccedcb679f2c0622e25e
Daniel S. Davis <daniel.davis@sageitsolutions.net>
2023-01-03 16:46:04 -0500

    Bugfix

--- Phalcon/Di/Service/Response/Json.php
+++ Phalcon/Di/Service/Response/Json.php
@@ -7,7 +7,7 @@ class Json implements \Phalcon\Di\ServiceProviderInterface
     public function register(\Phalcon\Di\DiInterface $di): void
     {
         $di->setShared(
-            'request',
+            'response',
             function () use ($di) {
                 $response = new \Phalcon\Http\Response();
                 $response->setContentType('application/json', 'utf-8');
